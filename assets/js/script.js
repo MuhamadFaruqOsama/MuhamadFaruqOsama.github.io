1bbac2972f76014a3457db32125db755f0fd9c5d
AskYourMom123 <abdolslamet@gmail.com>
2023-07-11 00:13:54 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+// sticky navbar
+window.addEventListener("scroll", function () {
+  var header = document.getElementById("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// active navbar
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+      });
+    }
+  });
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
+
+// menu icon
+let menuIcon = document.getElementById("menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
